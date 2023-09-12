13d9b4ed1246f55311ac70b1c9879000d2d1ab83
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-09-12 13:13:17 +0530

    Add files via upload

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,39 +1,20 @@
-def add(x, y):
-    return x + y
-
-
-def subtract(x, y):
-    return x - y
-
-
-def multiply(x, y):
-    return x * y
-
-
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero"
-    return x / y
-
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-choice = input("Enter choice (1/2/3/4): ")
-
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if choice == '1':
-    print("Result: ", add(num1, num2))
-elif choice == '2':
-    print("Result: ", subtract(num1, num2))
-elif choice == '3':
-    print("Result: ", multiply(num1, num2))
-elif choice == '4':
-    print("Result: ", divide(num1, num2))
+print('''
++ Addition
+- subtraction
+* Multiply
+/ Divide
+''')
+num1 = int(input("Enter the first value (integer type):: "))
+num2 = int(input("Enter the second value (integer type):: "))
+opr = input("Enter the operator(+,-,*,/):: ")
+if opr == "+":
+    print(num1 + num2)
+elif opr == "-":
+    print(num1 - num2)
+elif opr == "*":
+    print(num1 * num2)
+elif opr == "/":
+    print(num1 / num2)
 else:
-    print("Invalid input")
+    print("invalid operator !!!!")
+
