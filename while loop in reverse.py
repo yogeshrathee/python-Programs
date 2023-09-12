1512a58b29bfcdd78d0ae77e0222cf198c09e80b
yogesh <96315347+yogeshrathee@users.noreply.github.com>
2023-09-12 14:28:57 +0530

    Add files via upload

---
+++ while loop in reverse.py
@@ -0,0 +1,6 @@
+i = 10
+while i >= 0:
+    print(i, "reverse")
+    i = i - 1
+
+print(i)   # -1 because loop go to last value 0 ,then its go to -1, its false as based on condition, so its print -1
---
+++ while loop.py
@@ -0,0 +1,8 @@
+i = 1
+while i <= 10:
+    print(i, " :: Hello Yogesh")
+    i = i + 1
+
+print(i)  # 11 because loop go to last value 10 ,then its go to 11,its false as based on condition, so its print 11
+
+
